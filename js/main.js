457f3acd0f8b54c954a957c8d4cc274c5b3807d8
Dimatka <103488136+Dimatka@users.noreply.github.com>
2022-11-12 11:05:05 +0200

    Больше деталей (#3)
    Co-authored-by: Ruslan Breev <32988951+blckwltz@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,17 +1,66 @@
-function getRandomIntInclusive(min, max) {
-  if (min < 0 || max < 0) {
+// Генератор числа в диапазоне
+const NUMBER_OF_OBJECTS = 25;
+const getRandomNumber = (from, to) => {
+  if (from < 0 || to < 0) {
     return NaN;
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  if (Number.isInteger(from) && Number.isInteger(to)) {
+    if (from < to) {
+      return Math.floor(Math.random() * (to - from + 1)) + from;
+    } else {
+      return NaN;
+    }
+  } else {
+    return NaN;
+  }
+};
 
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
+const descriptionPhoto = [
+  'Пляж',
+  'Море',
+  'Кот-суши',
+  'Девушка',
+  'Крутая тачка',
+  'Концерт',
+  'Компотик',
+  'Отпуск',
+  'Полет'
+];
 
-getRandomIntInclusive(0, 1);
-checkStringLength('', 140);
+//Создание индекса массива описаний фотографий.
+const getArrayElement = () => getRandomNumber(0, descriptionPhoto.length - 1);
 
+// создание массива случайных, неповторяющихся чисел фиксированной длинны.
+const createArrayFixLength = () => {
+  const arrayUnicNumbers = [];
+  while (arrayUnicNumbers.length < NUMBER_OF_OBJECTS) {
+    const randomNumber = getRandomNumber(1, NUMBER_OF_OBJECTS);
+    if(arrayUnicNumbers.indexOf(randomNumber) === -1) {
+      arrayUnicNumbers.push(randomNumber);
+    }
+  }
+  return arrayUnicNumbers;
+};
 
+//Функция создающая один объект.
+const createPhoto = function(unicNumberId, unicNumberOfPhoto) {
+  return {
+    id: unicNumberId,
+    url: `photos/${unicNumberOfPhoto}.jpg`,
+    description: descriptionPhoto[getArrayElement()],
+    likes: getRandomNumber(15, 200),
+    comments: getRandomNumber(0, 200)
+  };
+};
+
+const photos = () => {
+  const arrayOfObjects = [];
+  const unicId = createArrayFixLength(NUMBER_OF_OBJECTS);
+  const unicNumberOfPhoto = createArrayFixLength(NUMBER_OF_OBJECTS);
+  for(let i = 0; i < NUMBER_OF_OBJECTS; i++) {
+    arrayOfObjects.push(createPhoto(unicId[i],unicNumberOfPhoto[i]));
+  }
+  return arrayOfObjects;
+};
+
+photos();
